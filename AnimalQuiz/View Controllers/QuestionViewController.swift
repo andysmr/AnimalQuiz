d0f46cea033deb120d0aa8958dfd238f23309f5c
andySmr <andrey180696007@yandex.ru>
2022-01-19 16:25:18 +0400

    version 1.6

--- AnimalQuiz/View Controllers/QuestionViewController.swift
+++ AnimalQuiz/View Controllers/QuestionViewController.swift
@@ -20,6 +20,10 @@ class QuestionViewController: UIViewController {
     @IBOutlet var multipleLabels: [UILabel]!
     @IBOutlet var rangedLabels: [UILabel]!
     
+    // MARK: Properties
+    private let questions = Question.getQuestions()
+    private var questionIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
@@ -33,6 +37,39 @@ extension QuestionViewController {
     private func updateUI() {
         for stackView in [singleStackView, multipleStackView, rangedStackView] {
             stackView?.isHidden = true
+        }
+        // get current question
+        let currentQuestion = questions[questionIndex]
+        
+        // set current question for label
+        questionLabel.text = currentQuestion.text
+        
+        // calculate progress
+        let totalProgress = Float(questionIndex) / Float(questions.count)
+        
+        // set progress for progressView
+        progressView.setProgress(totalProgress, animated: true)
+        
+        title = "Вопрос № \(questionIndex + 1) из \(questions.count)"
+        
+        // show current StackView
+    }
+    private func showCurrentStackView(for type: RessponseType) {
+        switch type {
+        case .single:
+            break
+        case .multiple:
+            break
+        case .range:
+            break
+        }
+    }
+    
+    private func showSingleStackView(with answers: [Answer]) {
+        singleStackView.isHidden = false
+        
+        for (button, answer) in zip(singleButtons, answers) {
+            button.setTitle(answer.text, for: .normal)
         }
     }
 }
