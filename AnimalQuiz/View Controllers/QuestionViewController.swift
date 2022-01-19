4f44a0c37ea53f8dc5ec8ac61803ea1f31047fa9
andySmr <andrey180696007@yandex.ru>
2022-01-19 14:58:32 +0400

    version 1.5

--- AnimalQuiz/View Controllers/QuestionViewController.swift
+++ AnimalQuiz/View Controllers/QuestionViewController.swift
@@ -8,4 +8,31 @@
 import UIKit
 class QuestionViewController: UIViewController {
     
+    @IBOutlet var progressView: UIProgressView!
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var rangedSlider: UISlider!
+    
+    @IBOutlet var singleStackView: UIStackView!
+    @IBOutlet var multipleStackView: UIStackView!
+    @IBOutlet var rangedStackView: UIStackView!
+    
+    @IBOutlet var singleButtons: [UIButton]!
+    @IBOutlet var multipleLabels: [UILabel]!
+    @IBOutlet var rangedLabels: [UILabel]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+}
+
+
+
+// MARK: Private
+extension QuestionViewController {
+    private func updateUI() {
+        for stackView in [singleStackView, multipleStackView, rangedStackView] {
+            stackView?.isHidden = true
+        }
+    }
 }
